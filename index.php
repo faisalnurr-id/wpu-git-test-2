aac0ec5a117c76329bdbf6f8af0b9fc484dbd4c9
Faisal Nur <faisalnurochman919@gmail.com>
2020-07-02 09:17:10 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
 	<h1>Hello World</h1>
 	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut at rerum, libero ipsam, porro similique quos corporis qui neque, dicta suscipit soluta aliquam non. Sit omnis cupiditate eum ratione recusandae!</p>
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
 </body>
 
 </html>
